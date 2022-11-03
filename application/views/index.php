f82bd0c4b29b22e8b4d148e59353720b6970cf43
haizaki66 <oxeo007@gmail.com>
2022-11-03 08:26:41 +0800

    first commit

---
+++ application/views/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+</head>
+
+<body>
+    <h1></h1>
+</body>
+
+</html>
---
+++ application/views/welcome.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>awdawdawd</title>
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+    <title>Halaman</title>
+    <nav class="navbar navbar-expand-lg bg-primary mb-4  border-top-0 ">
+        <div class="container">
+            <div class="container-fluid" style="display: flex;justify-content: space-between;">
+                <a class="navbar-brand text-white" href="#"><b>D3 RPL</b></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="text-white navbar-nav ">
+                        <a class="text-white nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="text-white nav-link" href="#">About</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+
+
+</head>
+
+<body style="
+    background: rgb(255,255,255);
+    background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(123,178,255,1) 100%);
+    background-attachment: fixed;
+">
+
+    </div>
+    <script src="<?php echo base_url("assets/js/bootstrap.js"); ?>"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+</body>
+</body>
+
+</html>
