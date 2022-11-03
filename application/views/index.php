bf19f44127d0df2719728cbbfc289f6127dd9627
haizaki66 <oxeo007@gmail.com>
2022-11-03 08:43:14 +0800

    Add NavBar & Alert

--- application/views/index.php
+++ application/views/index.php
@@ -7,12 +7,46 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Document</title>
-    
+
     <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 </head>
 
 <body>
-    <h1></h1>
+    <div class="container">
+
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Tugas</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tambah Data</a>
+                        </li>
+                    </ul>
+
+                    <div class="d-flex">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">ARI RAMADHAN</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+        </nav>
+
+
+        <div class="alert alert-info mt-4 text-center" role="alert">
+            <span class="fs-4">ARI RAMADHAN / 2001050007</span>
+        </div>
+
+    </div>
 </body>
 
 </html>
